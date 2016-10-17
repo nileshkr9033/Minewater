a6fbb73aa5f78569bba0194df7b07ccd09d38e7d
HQ-ICT-VINAYJ <hq-ict-vinayj@hq-ict-vinayjs-imac.local>
2016-10-17 12:03:43 +0530

    aj

---
+++ minewater1/AreaTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  AreaTableViewCell.swift
+//  minewater1
+//
+//  Created by HQ-ICT-VINAYJ on 17/10/16.
+//  Copyright © 2016 HQ-ICT-VINAYJ. All rights reserved.
+//
+
+import UIKit
+
+class AreaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var company: UILabel!
+    @IBOutlet weak var discharge: UILabel!
+    @IBOutlet weak var domestic: UILabel!
+    @IBOutlet weak var agriculture: UILabel!
+    @IBOutlet weak var industry: UILabel!
+   
+    @IBOutlet weak var recharge: UILabel!
+    @IBOutlet weak var projectuse: UILabel!
+    @IBOutlet weak var otheruse: UILabel!
+}
--- minewater1/AreaViewController.swift
+++ minewater1/AreaViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+class AreaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-class AreaViewController: UIViewController {
-
-    @IBOutlet weak var rightbutton: UIBarButtonItem!
+ 
+    @IBOutlet weak var tableView: UITableView!
+        @IBOutlet weak var rightbutton: UIBarButtonItem!
     @IBOutlet weak var leftbutton: UIBarButtonItem!
+    let model = generateRandomData()
+    var storedOffsets = [Int: CGFloat]()
     override func viewDidLoad() {
         super.viewDidLoad()
         if self.revealViewController() != nil {
@@ -25,7 +30,9 @@ class AreaViewController: UIViewController {
             
             print("Hello")
         }
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        getData()
         // Do any additional setup after loading the view.
     }
 
@@ -46,4 +53,130 @@ class AreaViewController: UIViewController {
     }
     */
 
+    struct Data
+    {
+        var company : String
+        var discharge : String
+        var projectuse : String
+        var otheruse : String
+    }
+    
+    var DataArray = [Data]()
+    var flag=0
+    
+    func getData()
+    {
+        
+        // print("_____")
+        
+        let url = "http://elib.cmpdi.co.in/MineWater/api/comp_ttl1.php"
+        //print(subsidiary1)
+        
+        
+        Alamofire.request(.POST, url)//, parameters: ["comp": subsidiary1!,"grade": grade1!,"mode": mode1!,"id": "41"])
+            .responseJSON { response in
+                if let res = response.result.value {
+                    let json = JSON(res)
+                    print("JSON \(json)")
+                    let count=json["result"].count
+                    //  print(json["result"][0]["COMP"])
+                    // print(count)
+                    var x=0;
+                    self.DataArray = [Data]()
+                    for i in 0..<count {
+                        print(i)
+                        
+                        
+                        let a:String=json["result"][i]["COMP"].stringValue
+                        //    let z:String=json["result"][i]["NOTICE_REFNO"].stringValue
+                        
+                        let b:String=json["result"][i]["DISCHARGE"].stringValue
+                        let c:String=json["result"][i]["OWN_USE"].stringValue
+                        let d:String=json["result"][i]["OTHER_USE"].stringValue
+                        //let e:String=json["result"][i]["TOTAL"].stringValue
+                        
+                        
+                        /*   if a == "ZCIL"{
+                         let domesticPercent = (self.roundToPlaces(((b as NSString).doubleValue/1000000),places: 2) / self.roundToPlaces(((e as NSString).doubleValue/1000000),places: 2) )*100
+                         self.domesticPercentage.text! = "\(domesticPercent)%"
+                         
+                         
+                         let agriPercent = (self.roundToPlaces(((c as NSString).doubleValue/1000000),places: 2) / self.roundToPlaces(((e as NSString).doubleValue/1000000),places: 2) )*100
+                         self.agriculturePercentage.text! = "\(agriPercent)%"
+                         
+                         
+                         let rechargePercent = (self.roundToPlaces(((d as NSString).doubleValue/1000000),places: 2) / self.roundToPlaces(((e as NSString).doubleValue/1000000),places: 2) )*100
+                         self.rechargePercentagaaage.text! = "\(rechargePercent)%"
+                         self.cilTotal = self.roundToPlaces(((e as NSString).doubleValue/1000000),places: 2)
+                         
+                         }else{
+                         self.sub1.append(a)
+                         self.float.append(self.roundToPlaces(((e as NSString).doubleValue/1000000),places: 2))
+                         }
+                         
+                         
+                         //   print("z is \(z)")
+                         */
+                        self.DataArray.append(Data(company: a, discharge: b, projectuse: c,otheruse: d))
+                        
+                    }
+                    
+                    
+                    self.flag=1
+                    self.tableView.reloadData()
+                    // self.chartLoad()
+                    
+                }
+                
+                
+        }
+        
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    func roundToPlaces(value:Double, places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return round(value * divisor) / divisor
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of feed items
+        
+        return DataArray.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Retrieve cell
+        let cellIdentifier: String = "Cell"
+        let myCell=self.tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as!
+        AreaTableViewCell
+        
+    
+        myCell.company.text! = DataArray[indexPath.row].company
+
+        
+        
+        
+        return myCell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Set selected location to var
+       // selectedLocation = feedItems[indexPath.row] as! LocationModel2
+        // Manually call segue to detail view controller
+        self.performSegueWithIdentifier("detailSegue1", sender: self)
+        
+    }
+    
 }
